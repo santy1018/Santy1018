c4abfd43d07cedf3e397233fe40d6ef642210de8
santy1018 <56858196+santy1018@users.noreply.github.com>
2019-10-22 14:43:40 -0500

    Add files via upload

---
+++ 04_if-vlan.py
@@ -0,0 +1,8 @@
+nativeVLAN=1
+dataVLAN=100
+if nativeVLAN==dataVLAN:
+    print("The nativeVlan and the dataVlan are the same."
+        )
+else:
+    print("The nativeVLAN and the dataVLAN are different.")
+    
