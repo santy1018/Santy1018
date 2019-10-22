e0ce2bbcb8dfda0ddc9e5eb6764c97a41a05df1f
santy1018 <56858196+santy1018@users.noreply.github.com>
2019-10-22 14:32:24 -0500

    Add files via upload

---
+++ 03_personal-info.py
@@ -0,0 +1,6 @@
+firstname=input("¿Cuál es tu nombre?")
+lastname=input("¿Cuál es tu apellido?")
+location=input("¿Donde te encuentras?")
+age=input("¿que edad tienes?")
+space=" "
+print("Bienvenido a la interfaz de python;",firstname,lastname,"te encuentras en:",location," y tienes",age,"años")
